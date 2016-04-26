52679e3f33c5b26380120c133513f5a62e926490
Felipe Gutiérrez <felgutier@gmail.com>
2016-04-26 04:38:58 -0300

    adding method on sender

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,14 +2,14 @@
   def self.provides_callback_for(provider)
     class_eval %Q{
       def #{provider}
-        @user = User.find_for_oauth(env["omniauth.auth"], current_user)
+        @sender = sender.find_for_oauth(env["omniauth.auth"], current_sender)
 
-        if @user.persisted?
-          sign_in_and_redirect @user, event: :authentication
+        if @sender.persisted?
+          sign_in_and_redirect @sender, event: :authentication
           set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
         else
           session["devise.#{provider}_data"] = env["omniauth.auth"]
-          redirect_to new_user_registration_url
+          redirect_to new_sender_registration_url
         end
       end
     }
--- app/models/sender.rb
+++ app/models/sender.rb
@@ -2,7 +2,7 @@ class Sender < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable,
-          :rememberable, :trackable, :validatable, :omniauthable#,omniauth_providers: [:facebook]
+          :rememberable, :trackable, :validatable, :omniauthable,omniauth_providers: [:facebook]
 
 
 
@@ -12,4 +12,56 @@ class Sender < ActiveRecord::Base
   has_many :identities, dependent: :destroy
 
   validates_inclusion_of :pay_day, :in => 1..31
+
+
+  def self.find_for_oauth(auth, signed_in_resource = nil)
+
+    # Get the identity and sender if they exist
+    identity = Identity.find_for_oauth(auth)
+
+    # If a signed_in_resource is provided it always overrides the existing sender
+    # to prevent the identity being locked with accidentally created accounts.
+    # Note that this may leave zombie accounts (with no associated identity) which
+    # can be cleaned up at a later date.
+    sender = signed_in_resource ? signed_in_resource : identity.sender
+
+    # Create the sender if needed
+    if sender.nil?
+
+      # Get the existing sender by email if the provider gives us a verified email.
+      # If no verified email was provided we assign a temporary email and ask the
+      # sender to verify it on the next step via sendersController.finish_signup
+      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+      email = auth.info.email if email_is_verified
+      sender = sender.where(email: email).first if email
+
+      # Create the sender if it's a new registration
+      if sender.nil?
+        if identity.provider == "twitter"
+            sender = sender.new(
+              name: auth.info.name,
+              email: email ? email : "update@me.com",
+              remote_avatar_url: auth.info.image,
+              password: Devise.friendly_token[0,20]
+            )
+          else
+            sender = sender.new(
+              name: auth.info.name,
+              email: email ? email : "update@me.com",
+              remote_avatar_url: auth.info.image,
+              password: Devise.friendly_token[0,20]
+            )
+          end
+        sender.save!
+      end
+    end
+
+    # Associate the identity with the sender if needed
+    unless identity.sender == sender
+      identity.sender = sender
+      identity.save!
+    end
+    sender
+  end
+
 end
