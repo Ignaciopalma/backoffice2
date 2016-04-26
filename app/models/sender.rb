cb4343bc623b41456e5116810faee8adf6fe6114
Felipe Gutiérrez <felgutier@gmail.com>
2016-04-26 04:28:55 -0300

    adding sender-identity relation

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -9,6 +9,7 @@ class Sender < ActiveRecord::Base
   belongs_to :area
   belongs_to :city
   has_many :deliveries
+  has_many :identities, dependent: :destroy
 
   validates_inclusion_of :pay_day, :in => 1..31
 end
