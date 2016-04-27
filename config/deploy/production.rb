111e22bd28c85a2a4d9ba47510634a56d8e28e53
Felipe Gutiérrez <felgutier@gmail.com>
2016-04-27 04:57:28 -0300

    adding staging branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 set :stage, :production
 set :rails_env, :production
 set :deploy_to, '/home/veloexpress/apps/veloexpress'
+set :branch, 'master'
 
 server '54.94.253.3', user: 'ubuntu', roles: %w{web app db}, primary: true
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
-set :stage, :staging
-set :rails_env, :staging
+set :stage, :production	
+set :rails_env, :production
 set :deploy_to, '/home/ubuntu/apps/veloexpress'
-
+set :branch, 'staging'
 
 server '52.35.248.254', user: 'ubuntu', roles: %w{web app db}, primary: true
 
