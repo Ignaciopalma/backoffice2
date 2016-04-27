84590443b0557b90bf2e5b228ace6efd70546eb6
Felipe Gutiérrez <felgutier@gmail.com>
2016-04-27 04:46:34 -0300

    change path deploy_to

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :rails_env, :staging
-set :deploy_to, '/home/ubuntu/apps/veloexpress'
+set :deploy_to, '/home/ubuntu/#{fetch(:application)'
 
 
 server '52.35.248.254', user: 'ubuntu', roles: %w{web app db}, primary: true
