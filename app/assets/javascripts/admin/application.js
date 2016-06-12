3b85c35fc71acfe648266a9a043782cb1228b601
Ignacio Palma <ignacio.palma.@live.com>
2016-06-12 16:50:05 -0300

    partializing correcto assets

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -79,3 +79,61 @@ $(function() {
   });
 });
 
+  $(document).ready(function () {
+    if(window.location.href.indexOf("admin/deliveries"))
+    {
+         $(".main-navigation li#deliveries").addClass("selected");
+    }
+    else if(window.location.href.indexOf("admin/users"))
+    {
+         $(".main-navigation li#users").addClass("selected");
+    }
+    else if(window.location.href.indexOf("admin/senders"))
+    {
+         $(".main-navigation li#senders").addClass("selected");
+    } 
+    
+});
+
+
+$(function() {
+
+
+// Create Select and append to menu
+  var $select = $("<select></select>");
+  $(".main-navigation").append($select);
+
+  // Cycle over menu links
+  $(".main-navigation a").each(function(){
+
+      var $anchor = $(this);
+      // Create Option
+      var $option = $("<option></option>");
+      // Option Value is the href
+      $option.val($anchor.attr("href"));
+      // Option text is the text of the link
+      $option.text($anchor.text());
+      // Append option to selct
+      $select.append($option);
+
+  });
+
+
+  // Create Button 
+  var $button = $("<button>Go</button>");
+
+  $(".main-navigation").append($button);
+  // Bind click to button
+  $button.click(function(){
+    //and go to select location
+    window.location = $select.val();
+
+  });
+
+  // Modify CSS to hide links on small resolution
+  // Also hide select and button on larger resolutions
+
+  // Deal with selected options
+});
+
+
